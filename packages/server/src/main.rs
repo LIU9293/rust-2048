b9cd170a22f9ad91e12399cc624c515157c99e03
liu9293 <liukai9293@qq.com>
2023-07-12 20:44:21 +0900

    change listen addr

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -62,8 +62,7 @@ async fn main() {
       )
       .with_state(Arc::clone(&app_state));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     axum::Server::bind(&addr)
       .serve(app.into_make_service())
       .await
